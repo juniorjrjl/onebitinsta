a6d9a0043eec44ca5ec783553aa37fbdb115a8fb
Junior <jose.junior@exactworks.com.br>
2021-01-05 18:30:25 -0300

    criaçao da estrutura inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,30 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'new-post',
+    loadChildren: () => import('./new-post/new-post.module').then( m => m.NewPostPageModule)
+  },
+  {
+    path: 'sign-in',
+    loadChildren: () => import('./sign-in/sign-in.module').then( m => m.SignInPageModule)
+  },
+  {
+    path: 'sign-up',
+    loadChildren: () => import('./sign-up/sign-up.module').then( m => m.SignUpPageModule)
+  },
+  {
+    path: 'sign-out',
+    loadChildren: () => import('./sign-out/sign-out.module').then( m => m.SignOutPageModule)
+  },
 ];
 
 @NgModule({
---
+++ src/app/sign-out/sign-out.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-out',
+  templateUrl: './sign-out.page.html',
+  styleUrls: ['./sign-out.page.scss'],
+})
+export class SignOutPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
